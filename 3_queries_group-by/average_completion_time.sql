0019d46565abd464a6a6cea11c7a5b66be84cbea
Daniel Tweedie <djtwd.ix@icloud.com>
2021-03-01 19:34:53 +0000

    Change col title to average_assignment_duration

--- 3_queries_group-by/average_completion_time.sql
+++ 3_queries_group-by/average_completion_time.sql
@@ -1,4 +1,4 @@
-SELECT students.name as student, AVG(assignment_submissions.duration)
+SELECT students.name as student, AVG(assignment_submissions.duration) as average_assignment_duration
 FROM assignment_submissions
 JOIN students ON students.id = student_id
 WHERE students.end_date IS NULL
