9c47c543ebc080c35669aade142b6cf2341f9b36
Daniel Tweedie <djtwd.ix@icloud.com>
2021-03-02 13:31:22 +0000

    Query for longest avg assistance time per cohort

---
+++ 4_queries/longest_average_assistance_cohort.sql
@@ -0,0 +1,7 @@
+SELECT cohorts.name, AVG(completed_at - started_at) as average_assistance_request_duration
+FROM assistance_requests
+JOIN students ON students.id = student_id
+JOIN cohorts on cohorts.id = cohort_id
+GROUP BY cohorts.name
+ORDER BY -AVG(completed_at - started_at)
+LIMIT 1;
